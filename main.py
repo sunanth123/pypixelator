c25fdd108d8cc9f47fbb8fcf6e517cb360587074
caameron <30531788+caameron@users.noreply.github.com>
2019-07-11 19:39:45 -0700

    added in main file and some test photos

---
+++ main.py
@@ -0,0 +1,29 @@
+# Libraries that we will be using to work with pictures mainly to just read in images and work with their pixels.
+import cv2
+from PIL import Image
+
+
+img = cv2.imread("beach.jpeg")
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+im = Image.open('beach.jpeg')
+size = im.size
+print(size);
+pixels = im.load()
+print(pixels[30,30])
+
+'''
+Using size we can loop through all the pixels and manipulate them however we wish
+'''
+for x in range(350):
+    pixels[x, 30] = (0,0,0)
+
+
+
+'''
+Have to install cv2 using pip install opencv-python
+and Pillow using pip install Pillow
+'''
