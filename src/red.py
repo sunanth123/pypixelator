1709a9f4c64eb66278dfebda4b95042dbf9cd74a
sunanth123 <sunanth@pdx.edu>
2019-08-01 15:39:48 -0700

    added comment red scale

--- src/red.py
+++ src/red.py
@@ -1,4 +1,4 @@
-## this function is responsible for converting an image into gray scale.
+## this function is responsible for converting an image into red scale.
 from PIL import Image
 
 def red(im):
@@ -12,7 +12,8 @@ def red(im):
     ## Iterate through each pixel of the original image and then place the pixel
     ## in the same position for the new image. The new pixel is an average of
     ## the red,green,blue pixel values of the original image. The resulting
-    ## average is then used as the new RGB values for the new pixel.
+    ## average is then used as the new RGB values for the new pixel, while
+    ## keeping the red value at its max value fo 255.
     for y in range(size[1]):
         for x in range(size[0]):
             single_pixel=im.getpixel((x,y))
